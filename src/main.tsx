7e488c5880de66f6d7e5f80b9efc1e6afec89c07
unknown <sptortarolo@gmail.com>
2026-05-14 15:25:08 -0300

    usuario global

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import { AuthProvider } from './hooks/useAuth';
+import { AuthProvider } from './hooks/AuthProvider';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
