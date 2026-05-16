fd07ab6685de4efc424e3bb28875db80e3e1a275
unknown <sptortarolo@gmail.com>
2026-05-16 16:50:46 -0300

    olvide la contraseña

--- server/auth.js
+++ server/auth.js
@@ -13,6 +13,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 export async function sendPasswordResetEmail(email, resetToken) {
