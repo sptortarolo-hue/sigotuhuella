03f6e3d82c73625b61652c4d6726c92c1ad3c29a
unknown <sptortarolo@gmail.com>
2026-06-23 11:04:07 -0300

    adoption broadcast: 4 veces/día (08,12,16,20)

--- server/index.js
+++ server/index.js
@@ -615,7 +615,7 @@ async function start() {
   async function checkAdoptionBroadcast() {
     try {
       const hour = new Date().getHours();
-      if ([0, 4, 8, 12, 16, 20].includes(hour)) {
+      if ([8, 12, 16, 20].includes(hour)) {
         await broadcastNextAdoptionPet();
         await broadcastFbAdoptionPets();
       }
